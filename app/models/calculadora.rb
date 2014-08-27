3a99250ad087f9cfd0e89c048faa49f258f5d8e0
Paulo Toro <pauloat@gmail.com>
2014-08-27 00:45:10 -0300

    comentarios en el modelo

--- app/models/calculadora.rb
+++ app/models/calculadora.rb
@@ -1,10 +1,12 @@
 # horas nocturnas SICA
-require 'chronic'
  
 class Calculadora
+  
+  # Public: Duracion en segundos de una jornada. 8h:45m 
   JORNADA = 31500
+  
+  # Public: Duracion en segundos de 12 hs.
   H12 = 2592000
- 
 
   attr_accessor :entradas, :salidas
  
@@ -13,27 +15,6 @@ class Calculadora
     @salidas = salidas.collect { |s| s.to_time }
   end
 
-  def am_4(entrada)
-    entrada.beginning_of_day + 4.hours
-  end
-
-  def pm_9(salida)
-    salida.beginning_of_day + 21.hours
-  end
-
-  def entrada_amanecer(entrada)
-    hora_nocturna am_4(entradas[n]) - entrada
-  end
- 
-  def salida_anochecer(salida)
-    hora_nocturna salida - pm_9(salidas[n])
-  end
- 
-  # tiempo es cantidad de segundos a convertir
-  def hora_nocturna(tiempo)
-    (tiempo / 50) * 60
-  end
- 
   def calcular(n)
     
     if entradas[n] < am_4(entradas[n])
@@ -73,15 +54,74 @@ class Calculadora
     end
 
     return Time.at(tiempo_extra).utc.strftime("%H:%M")
-
-#    def enganche(salida1, entrada2)
-#      descanso = entrada2 - salida1
-#      if descanso < H12
-#        horas_enganche = ((H12 - descanso).to_f) / 60 / 60
-#      else
-#        horas_enganche = 0
-#      end
-#    end
- 
   end
+
+#  def enganche(salida1, entrada2)
+#    descanso = entrada2 - salida1
+#    if descanso < H12
+#      horas_enganche = ((H12 - descanso).to_f) / 60 / 60
+#    else
+#      horas_enganche = 0
+#    end
+#  end
+
+  private
+
+  # Internal: Da las 4:00 del dia que se esta calculando
+  #
+  # entrada - El Datetime del que queremos sacar las 4:00 
+  #
+  # Examples
+  #
+  #   am_4(('2014-08-26T08:00').to_time)
+  #   # => '2014-08-26 04:00:00 -0300'
+  #
+  # Return un Time de las 4:00 del dia que se le dio
+  def am_4(entrada)
+    entrada.beginning_of_day + 4.hours
+  end
+  
+  # Internal: Da las 21:00 del dia que se esta calculando
+  #
+  # entrada - El Datetime del que queremos sacar las 21:00 
+  #
+  # Examples
+  #
+  #   pm_9(('2014-08-26T08:00').to_time)
+  #   # => '2014-08-26 21:00:00 -0300'
+  # 
+  # Return un Time de las 4:00 del dia que se le dio
+  def pm_9(salida)
+    salida.beginning_of_day + 21.hours
+  end
+
+  # Internal: Convierte cada 50 min de hora nocturna (desde las 21 hasta las 4)
+  # hora de 60 min.
+  #
+  # tiempo - Cantidad en segundos a convertir
+  #
+  # Examples
+  #
+  #   hora_nocturna(50)
+  #   # => '60'
+  #
+  #   hora_nocturna(2700)
+  #   # => '3240'
+  #
+  # Return el tiempo de hora nocturna regularizado a hora comun
+  def hora_nocturna(tiempo)
+    (tiempo / 50) * 60
+  end
+
+  # Internal: convierte a hora comun las horas nocturnas hasta las 4:00
+  #
+  # entrada - El Datetime que queremos calcular cuanto tiempo se trabajo antes de las 4:00
+  def entrada_amanecer(entrada)
+    hora_nocturna am_4(entrada) - entrada
+  end
+ 
+  def salida_anochecer(salida)
+    hora_nocturna salida - pm_9(salida)
+  end
+ 
 end
